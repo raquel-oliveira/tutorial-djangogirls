263b6578cfabed2764d210aad7b800692f241ee8
Raquel Oliveira <oliveira.raquel.lopes@gmail.com>
2018-10-18 16:51:18 -0300

    Delete filter of timezone (to show posts)

--- app/views.py
+++ app/views.py
@@ -6,5 +6,5 @@ from django.utils import timezone
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.order_by('published_date')
     return render(request, 'app/post_list.html', {'posts': posts})
